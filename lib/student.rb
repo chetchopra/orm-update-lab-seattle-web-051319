353c052cb92b0f368687ccff0bf76b1331306712
Chet  Chopra <chopra.chet@gmail.com>
2019-05-22 20:46:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,9 @@ class Student
       VALUES (?, ?)
     SQL
     
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql. self.name, self.grade)
     
-
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+  end
+  
 end
