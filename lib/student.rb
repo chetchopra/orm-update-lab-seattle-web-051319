bf8b19c4890aba149b90fd47265fd929436fd99c
Chet  Chopra <chopra.chet@gmail.com>
2019-05-22 20:41:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,6 +7,11 @@ class Student
   def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
-    
+    @id = id
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXSISTS
 
 end
