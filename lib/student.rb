4f93b976fad822f46450c05d7e3c641a8cc28fda
Chet  Chopra <chopra.chet@gmail.com>
2019-05-22 20:43:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,6 +12,13 @@ class Student
   
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE IF NOT EXSISTS
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade real);
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
 
 end
